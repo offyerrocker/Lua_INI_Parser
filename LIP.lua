f8299a1ab9f8f3fe7db36fad28743cbc0884cbdb
offyerrocker <offyerrocker88@gmail.com>
2022-09-28 15:09:00 -0700

    Allow sorting; Support inline comments from kasitoru
    Allow passing a table to sort the contents of each top-level table when saving an INI file. Sorting is applied identically in each case. Keys that are not specified will be sorted alphabetically or ascending by default.
    If a sort table is not provided, contents will not be sorted at all, reverting entirely to previous behavior.
    Support inline comments- Merged pr from original repo, from user kasitoru https://github.com/Dynodzzo/Lua_INI_Parser/pull/2/commits/addf72fcc01274ca72d00b61fd390d06f2191662
    Removed min file version because I don't currently feel a need to maintain it

--- LIP.lua
+++ LIP.lua
@@ -1,3 +1,7 @@
+-- original file from https://github.com/Dynodzzo/Lua_INI_Parser/pull/2/commits/addf72fcc01274ca72d00b61fd390d06f2191662
+
+--edited by offyerrocker to sort alphabetically or by provided order table
+
 --[[
 	Copyright (c) 2012 Carreras Nicolas
 	
@@ -39,8 +43,9 @@ function LIP.load(fileName)
 			section = tonumber(tempSection) and tonumber(tempSection) or tempSection;
 			data[section] = data[section] or {};
 		end
-		local param, value = line:match('^([%w|_]+)%s-=%s-(.+)$');
+		local param, value = line:match('^([%w|_]+)%s*=%s*(.*)[\r]?$');
 		if(param and value ~= nil)then
+			value = value:match('^(.-)%s-;.-$') or value:match('^(.-)%s-$');
 			if(tonumber(value))then
 				value = tonumber(value);
 			elseif(value == 'true')then
@@ -61,17 +66,45 @@ end
 --- Saves all the data from a table to an INI file.
 --@param fileName The name of the INI file to fill. [string]
 --@param data The table containing all the data to store. [table]
-function LIP.save(fileName, data)
+function LIP.save(fileName, data, sort)
 	assert(type(fileName) == 'string', 'Parameter "fileName" must be a string.');
 	assert(type(data) == 'table', 'Parameter "data" must be a table.');
 	local file = assert(io.open(fileName, 'w+b'), 'Error loading file :' .. fileName);
 	local contents = '';
-	for section, param in pairs(data) do
-		contents = contents .. ('[%s]\n'):format(section);
-		for key, value in pairs(param) do
-			contents = contents .. ('%s=%s\n'):format(key, tostring(value));
+	if(sort and type(sort) == "table")then 
+		for section, param in pairs(data) do
+			--add keys in given sorted order
+			contents = contents .. ('[%s]\n'):format(section);
+			for i,key in ipairs(sort) do
+				local value = param[key];
+				if(value ~= nil) then --must be able to distinguish between false/nil
+					contents = contents .. ('%s=%s\n'):format(key, tostring(value));
+				end
+			end
+			--any unsorted pairs (ie not listed in the provided sort table) are then added alphabetically at the end
+			local unsorted_values = {};
+			for key,value in pairs(param) do 
+				if not(table.contains(sort,key))then 
+					table.insert(unsorted_values,key);
+				end
+			end
+			table.sort(unsorted_values,function(a,b) return a < b end)
+			for _,key in ipairs(unsorted_values) do 
+				local value = param[key]
+				if(value ~= nil)then
+					contents = contents .. ('%s=%s\n'):format(key, tostring(value)) .. '\n';
+				end
+			end
+			contents = contents .. '\n';
 		end
-		contents = contents .. '\n';
+	else
+		for section, param in pairs(data) do
+			contents = contents .. ('[%s]\n'):format(section);
+			for key, value in pairs(param) do
+				contents = contents .. ('%s=%s\n'):format(key, tostring(value));
+			end
+			contents = contents .. '\n';
+		end
 	end
 	file:write(contents);
 	file:close();
